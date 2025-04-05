c6007d83645f5e2a55545ee07cd963db1fac0695
finalsummer <642661520@qq.com>
2025-04-05 22:52:54 +0800

    feat(components): 新增render-v-node组件用于动态渲染VNode
    新增了一个名为`render-v-node`的Vue组件，用于根据传入的`dynamicVNode`属性动态渲染VNode。该组件支持字符串、VNode对象以及返回VNode的函数作为输入，提高了代码的灵活性和复用性。

---
+++ packages/ol/components/common/render-v-node.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { defineComponent, h, isVNode } from 'vue';
+  import type { PropType, VNodeChild } from 'vue';
+  export default defineComponent({
+    props: {
+      dynamicVNode: {
+        type: [Function, String, Object] as PropType<
+          (() => VNodeChild) | VNodeChild | string>,
+        default: undefined,
+      },
+    },
+    setup(props) {
+      return () => {
+        if (!props.dynamicVNode) return h('span');
+        if (typeof props.dynamicVNode === 'string') return props.dynamicVNode;
+        if (isVNode(props.dynamicVNode)) return props.dynamicVNode;
+        if (typeof props.dynamicVNode === 'function') {
+          const vNode = props.dynamicVNode();
+          if (isVNode(vNode)) return vNode;
+          return vNode;
+        }
+      };
+    },
+  });
+</script>
