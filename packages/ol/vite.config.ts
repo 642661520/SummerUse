d7ed904c08793bdd376e66540ee8bbec64c6a9d7
finalsummer <642661520@qq.com>
2025-04-05 22:33:44 +0800

    build(vite): 更新外部依赖配置以支持更多模块
    将 `external` 配置中的 `'ol'` 修改为正则表达式 `/^ol.*/`，并添加 `/^@turf.*/`，以确保所有以 `ol` 和 `@turf` 开头的模块都被外部化处理，避免打包进库。

--- packages/ol/vite.config.ts
+++ packages/ol/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     rollupOptions: {
       // 确保外部化处理那些
       // 你不想打包进库的依赖
-      external: ['vue', 'naive-ui', 'ol'],
+      external: ['vue', 'naive-ui', /^ol.*/, /^@turf.*/],
       output: [
         {
           format: 'es',
