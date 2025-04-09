8deb14f793252299ac5eac16755b8ba07503eb28
finalsummer <642661520@qq.com>
2025-04-09 15:45:52 +0800

    docs(projection): 添加坐标系转换的文档和示例代码
    添加了关于坐标系转换的文档和示例代码，包括WGS84坐标与墨卡托坐标之间的转换。文档中包含了示例代码的展示和源代码的链接，方便开发者理解和测试。

---
+++ packages/ol/utils/projection/demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <NFormItem label="WGS84坐标转墨卡托">
+    <NInputNumber v-model:value="coord[0]" :step="0.1" /> 经度
+    <NInputNumber v-model:value="coord[1]" :step="0.1" /> 纬度
+  </NFormItem>
+  <NFormItem label="=">
+    <span> {{ mercator[0] }} , {{ mercator[1] }} </span>
+  </NFormItem>
+  <NFormItem label="墨卡托转WGS84">
+    <NInputNumber v-model:value="mercator2[0]" :step="0.1" /> x
+    <NInputNumber v-model:value="mercator2[1]" :step="0.1" /> y
+  </NFormItem>
+  <NFormItem label="=">
+    <span> {{ coord2[0] }} , {{ coord2[1] }} </span>
+  </NFormItem>
+</template>
+<script lang="ts" setup>
+  import { NFormItem, NInputNumber } from 'naive-ui'
+  import { wgs84ToMercator, mercatorToWgs84 } from '@summeruse/ol'
+  import { ref } from 'vue';
+  import { computed } from 'vue';
+  const coord = ref([116.404, 39.915]);
+
+  const mercator = computed(() => wgs84ToMercator(coord.value));
+
+  const mercator2 = ref([12959638.8957, 4855982.5492]);
+  const coord2 = computed(() => mercatorToWgs84(mercator2.value));
+
+</script>
---
+++ packages/ol/utils/projection/index.md
@@ -0,0 +1,19 @@
+# 投影
+
+<script lang="ts" setup>
+import Demo from './demo.vue';
+</script>
+
+## 坐标系转换
+
+<Demo />
+
+::: details 点我查看代码
+<<<./demo.vue
+:::
+
+## 源代码
+
+::: details 点我查看代码
+<<<./index.ts
+:::
