cbf0c9d005821cb85bc05f55ebc736ed892bcc2f
finalsummer <642661520@qq.com>
2025-04-09 15:45:25 +0800

    docs(utils/distance): 添加距离单位转换的文档和示例
    新增了距离单位转换的文档和示例代码，包括公里与海里的相互转换功能。文档中提供了使用示例和源代码链接，便于开发者理解和使用该功能。

---
+++ packages/ol/utils/distance/demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <NFormItem label="公里转海里">
+    <NInputNumber v-model:value="km" :min="0" /> 公里
+    <span> = {{ nauticalMiles }} 海里 </span>
+  </NFormItem>
+  <NFormItem label="海里转公里">
+    <NInputNumber v-model:value="nauticalMiles2" :min="0" /> 海里公里
+    <span> = {{ km2 }} 海里 </span>
+  </NFormItem>
+</template>
+<script lang="ts" setup>
+  import { NFormItem, NInputNumber } from 'naive-ui'
+  import { kmToNauticalMiles, nauticalMilesToKm } from '@summeruse/ol'
+  import { ref } from 'vue';
+  import { computed } from 'vue';
+  const km = ref(10);
+  const nauticalMiles = computed(() => kmToNauticalMiles(km.value).toFixed(2));
+
+  const nauticalMiles2 = ref(5.4);
+  const km2 = computed(() => nauticalMilesToKm(nauticalMiles2.value).toFixed(2));
+
+</script>
---
+++ packages/ol/utils/distance/index.md
@@ -0,0 +1,19 @@
+# 距离单位
+
+<script lang="ts" setup>
+import Demo from './demo.vue';
+</script>
+
+## 公里/海里转换
+
+<Demo />
+
+::: details 点我查看代码
+<<<./demo.vue
+:::
+
+## 源代码
+
+::: details 点我查看代码
+<<<./index.ts
+:::
