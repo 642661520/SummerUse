58c1e996e3289546543f6ec879747826115c676a
finalsummer <642661520@qq.com>
2025-04-09 10:15:46 +0800

    docs(projection): 添加坐标系相关函数的注释
    为投影相关的常量和函数添加中文注释，提高代码的可读性和可维护性

--- packages/ol/constants/projection.ts
+++ packages/ol/constants/projection.ts
@@ -1,7 +1,11 @@
+/** 3857坐标系 墨卡托坐标系 */
 export const EPSG_3857 = 'EPSG:3857';
 
+/** 4326坐标系 经纬度坐标系 */
 export const EPSG_4326 = 'EPSG:4326';
 
+/** 3857坐标系 墨卡托坐标系 */
 export const WebMercatorProjection = EPSG_3857;
 
+/** 4326坐标系 经纬度坐标系 */
 export const WGS84Projection = EPSG_4326;
--- packages/ol/utils/projection/index.ts
+++ packages/ol/utils/projection/index.ts
@@ -7,18 +7,27 @@ import type { Extent } from 'ol/extent';
 export const wgs84ToMercator = (coordinate: Coordinate) => {
   return transform(coordinate, EPSG_4326, EPSG_3857);
 };
+/** WGS84坐标转墨卡托  */
+export const EPSG_4326ToEPSG_3857 = wgs84ToMercator;
 
 /** 墨卡托坐标转WGS84 */
 export const mercatorToWgs84 = (coordinate: Coordinate) => {
   return transform(coordinate, EPSG_3857, EPSG_4326);
 };
+/** 墨卡托坐标转WGS84 */
+export const EPSG_3857ToEPSG_4326 = mercatorToWgs84;
 
 /** WGS84范围转墨卡托 */
 export const wgs84ExtentToMercator = (extent: Extent) => {
   return transformExtent(extent, EPSG_4326, EPSG_3857);
 };
+/** WGS84范围转墨卡托 */
+export const EPSG_4326ExtentToEPSG_3857 = wgs84ExtentToMercator;
 
 /** 墨卡托范围转WGS84 */
 export const mercatorExtentToWgs84 = (extent: Extent) => {
   return transformExtent(extent, EPSG_3857, EPSG_4326);
 };
+
+/** 墨卡托范围转WGS84 */
+export const EPSG_3857ExtentToEPSG_4326 = mercatorExtentToWgs84;
