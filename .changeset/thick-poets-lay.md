048ca5de651d43d84cb031488f3f7b738d8c21ac
finalsummer <642661520@qq.com>
2025-04-09 10:15:10 +0800

    docs(utils/layer): 更新天地图类型的注释
    添加了对天地图类型的详细注释，包括矢量底图、矢量注记、影像底图、影像注记、地形晕渲、地形注记和全球境界的说明

---
+++ .changeset/thick-poets-lay.md
@@ -0,0 +1,5 @@
+---
+"@summeruse/ol": patch
+---
+
+修复：天地图类型
--- packages/ol/utils/layer/index.ts
+++ packages/ol/utils/layer/index.ts
@@ -3,7 +3,17 @@ import type { ProjectionLike } from 'ol/proj';
 import { BingMaps, OSM, XYZ } from 'ol/source';
 import { WebMercatorProjection, WGS84Projection } from '../../constants/projection';
 
-export type T_MAP_TYPE = 'vec' | 'cva' | 'img' | 'ter' | 'cia' | 'eia';
+/**
+ * 天地图类型
+ * vec: 矢量底图
+ * cva: 矢量注记
+ * img: 影像底图
+ * cia: 影像注记
+ * ter: 地形晕渲
+ * cta: 地形注记
+ * ibo: 全球境界
+ */
+export type T_MAP_TYPE = 'vec' | 'cva' | 'img' | 'cia' | 'ter' | 'cta' | 'ibo';
 
 export const getTianDiTuUrl = (data: {
   url?: string;
