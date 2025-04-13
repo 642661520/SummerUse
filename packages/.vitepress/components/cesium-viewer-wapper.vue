d58073de324cf32fcfed779f992b954f54738a7b
finalsummer <642661520@qq.com>
2025-04-13 16:16:54 +0800

    feat(组件): 添加CesiumViewerWrapper组件以封装CesiumViewer
    新增CesiumViewerWrapper组件，用于封装CesiumViewer，提供统一的样式和插槽功能，便于在项目中复用

---
+++ packages/.vitepress/components/cesium-viewer-wapper.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { CesiumViewer } from '@summeruse/cesium'
+
+defineOptions({
+  name: 'CesiumViewerWrapper',
+})
+</script>
+
+<template>
+  <CesiumViewer class="w-100% h-300px">
+    <template #default="{ viewer }">
+      <slot :viewer="viewer" />
+    </template>
+  </CesiumViewer>
+</template>
