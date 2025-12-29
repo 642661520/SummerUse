a3138b408c631cfc40e235e068d3f1651431d6ad
final summer <642661520@qq.com>
2025-12-29 15:13:30 +0800

    feat(投影): 添加EPSG:3395椭球墨卡托投影支持
    新增EPSG:3395椭球墨卡托投影坐标系定义，并注册到OpenLayers中
    添加createTileGrid函数用于创建对应投影的瓦片网格
    完善投影类型定义ProjectionLike

---
+++ .changeset/wild-schools-end.md
@@ -0,0 +1,5 @@
+---
+"@summeruse/ol": patch
+---
+
+feat(投影): 添加 EPSG:3395 椭球墨卡托投影支持
--- packages/ol/constants/projection.ts
+++ packages/ol/constants/projection.ts
@@ -1,13 +1,32 @@
-/** 3857坐标系 墨卡托坐标系 */
+import type { Projection } from 'ol/proj'
+import { register } from 'ol/proj/proj4'
+import { createXYZ } from 'ol/tilegrid'
+import proj4 from 'proj4'
+
+/** 3857坐标系 球面墨卡托投影坐标系 */
 export const EPSG_3857 = 'EPSG:3857'
 
-/** 4326坐标系 经纬度坐标系 */
+/** 4326坐标系 经纬度坐标系 等距圆柱投影 */
 export const EPSG_4326 = 'EPSG:4326'
 
-/** 3857坐标系 墨卡托坐标系 */
-export const WebMercatorProjection = EPSG_3857
+/** 3395坐标系 椭球墨卡托投影坐标系 */
+export const EPSG_3395 = 'EPSG:3395'
 
-/** 4326坐标系 经纬度坐标系 */
-export const WGS84Projection = EPSG_4326
+export type ProjectionLike = 'EPSG:3857' | 'EPSG:4326' | 'EPSG:3395'
 
-export type ProjectionLike = 'EPSG:3857' | 'EPSG:4326'
+proj4.defs(
+  'EPSG:3395',
+  // cSpell:disable-next-line
+  '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 ' + '+datum=WGS84 +units=m +no_defs +type=crs',
+)
+
+// 注册到 OpenLayers
+register(proj4)
+
+export function createTileGrid(ProjectionLike?: Projection | string) {
+  if (ProjectionLike === EPSG_3395) {
+    return createXYZ({
+      extent: [-20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892],
+    })
+  }
+}
