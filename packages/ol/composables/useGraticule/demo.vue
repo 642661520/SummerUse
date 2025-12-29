08d2630f7212fd4f041c94d3db01b6cf11d8499d
final summer <642661520@qq.com>
2025-12-29 17:31:40 +0800

    feat(useGraticule): 添加第二个网格图层并合并显示控制
    添加第二个网格图层(showGraticule2)并创建计算属性合并两个图层的显示控制

--- packages/ol/composables/useGraticule/demo.vue
+++ packages/ol/composables/useGraticule/demo.vue
@@ -2,11 +2,12 @@
 import { createOpenStreetMapLayer, OlMap, useGraticule } from '@summeruse/ol'
 import { NButton } from 'naive-ui'
 import { Map as OLMap } from 'ol'
+import { computed } from 'vue'
 
 const olMap = new OLMap()
 const layer = createOpenStreetMapLayer()
 olMap.addLayer(layer)
-const { showGraticule } = useGraticule({
+const { showGraticule: showGraticule1 } = useGraticule({
   olMap,
   graticuleOptions: {
     maxLines: 20,
@@ -16,8 +17,44 @@ const { showGraticule } = useGraticule({
       lineDash: [10, 10],
     },
     showLabels: true,
+
   },
   defaultShow: true,
+})
+
+const { showGraticule: showGraticule2 } = useGraticule({
+  olMap,
+  graticuleOptions: {
+    lonLabelPosition: 1,
+    latLabelPosition: 0,
+    latLabelStyleOption: {
+      font: '12px Calibri,sans-serif',
+      textAlign: 'left',
+      strokeOptions: {
+        color: 'rgba(255,255,255,1)',
+        width: 3,
+      },
+    },
+    lonLabelStyleOption: {
+      font: '12px Calibri,sans-serif',
+      textBaseline: 'top',
+      strokeOptions: {
+        color: 'rgba(255,255,255,1)',
+        width: 3,
+      },
+    },
+    showLabels: true,
+    opacity: 0,
+  },
+  defaultShow: true,
+})
+
+const showGraticule = computed({
+  get: () => showGraticule1.value || showGraticule2.value,
+  set: (val) => {
+    showGraticule1.value = val
+    showGraticule2.value = val
+  },
 })
 </script>
 
